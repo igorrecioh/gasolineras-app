77f962cbba8d1c6f13ece702df414f5997779a7b
Igor Recio <igor.recio.h@gmail.com>
2021-10-04 20:01:38 +0200

    Renaming de variables, refactor y cambios en linter

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import gas
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        self.ui_window = Ui_MainWindow()
+        self.ui_window.setupUi(self)
 
         # Variables
         self.provinces = {}
@@ -18,152 +18,154 @@ class MainWindow(QtWidgets.QMainWindow):
         self.stations = []
 
         # Buttons
-        self.ui.salirBtn.clicked.connect(self.close)
+        self.ui_window.salirBtn.clicked.connect(self.close)
 
         # Comboboxes
-        self.ui.comunidadCB.addItem("Seleccione CCAA")
-        self.ui.comunidadCB.model().item(0).setEnabled(False)
-        self.ui.comunidadCB.setCurrentIndex(0)
-        self.ui.comunidadCB.currentTextChanged.connect(self.ccaa_selected)
+        self.ui_window.comunidadCB.addItem("Seleccione CCAA")
+        self.ui_window.comunidadCB.model().item(0).setEnabled(False)
+        self.ui_window.comunidadCB.setCurrentIndex(0)
+        self.ui_window.comunidadCB.currentTextChanged.connect(self.ccaa_selected)
         self.comunidades = gas.get_comunidades()
 
         if self.comunidades == {}:
             self.statusBar().showMessage("Error de conexión, salga de la aplicación y reinténtelo")
         else:
             for comunidad in self.comunidades:
-                self.ui.comunidadCB.addItem(comunidad)
+                self.ui_window.comunidadCB.addItem(comunidad)
 
-        self.ui.provinciaCB.addItem("Seleccione Provincia")
-        self.ui.provinciaCB.setEnabled(False)
-        self.ui.provinciaCB.model().item(0).setEnabled(False)
-        self.ui.provinciaCB.setCurrentIndex(0)
-        self.ui.provinciaCB.currentTextChanged.connect(self.province_selected)
+        self.ui_window.provinciaCB.addItem("Seleccione Provincia")
+        self.ui_window.provinciaCB.setEnabled(False)
+        self.ui_window.provinciaCB.model().item(0).setEnabled(False)
+        self.ui_window.provinciaCB.setCurrentIndex(0)
+        self.ui_window.provinciaCB.currentTextChanged.connect(self.province_selected)
 
-        self.ui.municipioCB.addItem("Seleccione Municipio")
-        self.ui.municipioCB.setEnabled(False)
-        self.ui.municipioCB.model().item(0).setEnabled(False)
-        self.ui.municipioCB.setCurrentIndex(0)
-        self.ui.municipioCB.currentTextChanged.connect(self.city_selected)
+        self.ui_window.municipioCB.addItem("Seleccione Municipio")
+        self.ui_window.municipioCB.setEnabled(False)
+        self.ui_window.municipioCB.model().item(0).setEnabled(False)
+        self.ui_window.municipioCB.setCurrentIndex(0)
+        self.ui_window.municipioCB.currentTextChanged.connect(self.city_selected)
 
-        self.ui.direccionCB.addItem("Seleccione Gasolinera")
-        self.ui.direccionCB.setEnabled(False)
-        self.ui.direccionCB.model().item(0).setEnabled(False)
-        self.ui.direccionCB.setCurrentIndex(0)
-        self.ui.direccionCB.currentTextChanged.connect(self.station_selected)
+        self.ui_window.direccionCB.addItem("Seleccione Gasolinera")
+        self.ui_window.direccionCB.setEnabled(False)
+        self.ui_window.direccionCB.model().item(0).setEnabled(False)
+        self.ui_window.direccionCB.setCurrentIndex(0)
+        self.ui_window.direccionCB.currentTextChanged.connect(self.station_selected)
 
     def ccaa_selected(self):
-        print(self.ui.comunidadCB.currentText())
-        self.ui.provinciaCB.clear()
-        self.ui.municipioCB.setEnabled(False)
+        print(self.ui_window.comunidadCB.currentText())
+        self.ui_window.provinciaCB.clear()
+        self.ui_window.municipioCB.setEnabled(False)
 
         self.provinces = \
-            gas.get_provincias(self.comunidades.get(self.ui.comunidadCB.currentText()))
-        self.ui.provinciaCB.addItem("Seleccione Provincia")
-        self.ui.municipioCB.addItem("Seleccione Municipio")
+            gas.get_provincias(self.comunidades.get(self.ui_window.comunidadCB.currentText()))
+        self.ui_window.provinciaCB.addItem("Seleccione Provincia")
+        self.ui_window.municipioCB.addItem("Seleccione Municipio")
 
         for province in self.provinces:
-            self.ui.provinciaCB.addItem(province)
+            self.ui_window.provinciaCB.addItem(province)
 
-        self.ui.provinciaCB.setEnabled(True)
+        self.ui_window.provinciaCB.setEnabled(True)
 
     def province_selected(self):
-        print("Provincia: " + self.ui.provinciaCB.currentText())
-        self.ui.municipioCB.clear()
+        print("Provincia: " + self.ui_window.provinciaCB.currentText())
+        self.ui_window.municipioCB.clear()
 
-        if self.ui.provinciaCB.currentIndex() != -1 and self.ui.provinciaCB.currentIndex() != 0:
-            self.cities = gas.get_municipios(self.provinces.get(self.ui.provinciaCB.currentText()))
-            self.ui.municipioCB.addItem("Seleccione Municipio")
+        if self.ui_window.provinciaCB.currentIndex() != -1 and self.ui_window.provinciaCB.currentIndex() != 0:
+            self.cities = gas.get_municipios(self.provinces.get(self.ui_window.provinciaCB.currentText()))
+            self.ui_window.municipioCB.addItem("Seleccione Municipio")
 
             for city in self.cities:
-                self.ui.municipioCB.addItem(city)
+                self.ui_window.municipioCB.addItem(city)
 
-            self.ui.municipioCB.setEnabled(True)
+            self.ui_window.municipioCB.setEnabled(True)
 
     def city_selected(self):
-        print("Municipio: " + self.ui.municipioCB.currentText())
-        self.ui.direccionCB.clear()
+        print("Municipio: " + self.ui_window.municipioCB.currentText())
+        self.ui_window.direccionCB.clear()
 
-        if self.ui.municipioCB.currentIndex() != -1 and self.ui.municipioCB.currentIndex() != 0:
+        if self.ui_window.municipioCB.currentIndex() != -1 and self.ui_window.municipioCB.currentIndex() != 0:
             self.stations = gas.get_estaciones_por_ciudad(
-                self.cities.get(self.ui.municipioCB.currentText()))
-            self.ui.direccionCB.addItem("Seleccione Gasolinera")
+                self.cities.get(self.ui_window.municipioCB.currentText()))
+            self.ui_window.direccionCB.addItem("Seleccione Gasolinera")
 
             for station in self.stations:
                 print(station)
-                self.ui.direccionCB\
+                self.ui_window.direccionCB\
                     .addItem(str(station['Rótulo']) + " - " + str(station['Dirección']))
 
-            self.ui.direccionCB.setEnabled(True)
+            self.ui_window.direccionCB.setEnabled(True)
 
     def station_selected(self):
-        self.ui.sp95E5LCD.display(0.0)
-        self.ui.sp98E5LCD.display(0.0)
-        self.ui.sp95E10LCD.display(0.0)
-        self.ui.sp95E5PremiumLCD.display(0.0)
-        self.ui.sp98E10LCD.display(0.0)
-
-        self.ui.gasoleoALCD.display(0.0)
-        self.ui.gasoleoBLCD.display(0.0)
-        self.ui.gasoleoPremiumLCD.display(0.0)
-
-        self.ui.biodieselLCD.display(0.0)
-        self.ui.bioetanolLCD.display(0.0)
-
-        self.ui.gncLCD.display(0.0)
-        self.ui.gnlLCD.display(0.0)
-        self.ui.glpLCD.display(0.0)
-        self.ui.hidrogenoLCD.display(0.0)
-        if self.ui.direccionCB.currentIndex() != -1 and self.ui.direccionCB.currentIndex() != 0:
-            station = self.stations[self.ui.direccionCB.currentIndex() - 1]
+        self.reset_displays()
+        if self.ui_window.direccionCB.currentIndex() != -1 and self.ui_window.direccionCB.currentIndex() != 0:
+            station = self.stations[self.ui_window.direccionCB.currentIndex() - 1]
             if station['Precio Gasolina 95 E5'] != '':
-                self.ui.sp95E5LCD.display(float(station['Precio Gasolina 95 E5'].replace(',', '.')))
+                self.ui_window.sp95E5LCD.display(float(station['Precio Gasolina 95 E5'].replace(',', '.')))
 
             if station['Precio Gasolina 98 E5'] != '':
-                self.ui.sp98E5LCD.display(float(station['Precio Gasolina 98 E5'].replace(',', '.')))
+                self.ui_window.sp98E5LCD.display(float(station['Precio Gasolina 98 E5'].replace(',', '.')))
 
             if station['Precio Gasolina 95 E5 Premium'] != '':
-                self.ui.sp95E5PremiumLCD.display(
+                self.ui_window.sp95E5PremiumLCD.display(
                     float(station['Precio Gasolina 95 E5 Premium'].replace(',', '.')))
 
             if station['Precio Gasolina 95 E10'] != '':
-                self.ui.sp95E10LCD.display(
+                self.ui_window.sp95E10LCD.display(
                     float(station['Precio Gasolina 95 E10'].replace(',', '.')))
 
             if station['Precio Gasolina 98 E10'] != '':
-                self.ui.sp98E10LCD.display(
+                self.ui_window.sp98E10LCD.display(
                     float(station['Precio Gasolina 98 E10'].replace(',', '.')))
 
             if station['Precio Gasoleo A'] != '':
-                self.ui.gasoleoALCD.display(float(station['Precio Gasoleo A'].replace(',', '.')))
+                self.ui_window.gasoleoALCD.display(float(station['Precio Gasoleo A'].replace(',', '.')))
 
             if station['Precio Gasoleo B'] != '':
-                self.ui.gasoleoBLCD.display(float(station['Precio Gasoleo B'].replace(',', '.')))
+                self.ui_window.gasoleoBLCD.display(float(station['Precio Gasoleo B'].replace(',', '.')))
 
             if station['Precio Gasoleo Premium'] != '':
-                self.ui.gasoleoPremiumLCD.display(
+                self.ui_window.gasoleoPremiumLCD.display(
                     float(station['Precio Gasoleo Premium'].replace(',', '.')))
 
             if station['Precio Biodiesel'] != '':
-                self.ui.biodieselLCD.display(float(station['Precio Biodiesel'].replace(',', '.')))
+                self.ui_window.biodieselLCD.display(float(station['Precio Biodiesel'].replace(',', '.')))
 
             if station['Precio Bioetanol'] != '':
-                self.ui.bioetanolLCD.display(float(station['Precio Bioetanol'].replace(',', '.')))
+                self.ui_window.bioetanolLCD.display(float(station['Precio Bioetanol'].replace(',', '.')))
 
             if station['Precio Gas Natural Comprimido'] != '':
-                self.ui.gncLCD.display(
+                self.ui_window.gncLCD.display(
                     float(station['Precio Gas Natural Comprimido'].replace(',', '.')))
 
             if station['Precio Gas Natural Licuado'] != '':
-                self.ui.gnlLCD.display(
+                self.ui_window.gnlLCD.display(
                     float(station['Precio Gas Natural Licuado'].replace(',', '.')))
 
             if station['Precio Gases licuados del petróleo'] != '':
-                self.ui.glpLCD.display(
+                self.ui_window.glpLCD.display(
                     float(station['Precio Gases licuados del petróleo'].replace(',', '.')))
 
             if station['Precio Hidrogeno'] != '':
-                self.ui.hidrogenoLCD.display(float(station['Precio Hidrogeno'].replace(',', '.')))
+                self.ui_window.hidrogenoLCD.display(float(station['Precio Hidrogeno'].replace(',', '.')))
 
+    def reset_displays(self):
+        self.ui_window.sp95E5LCD.display(0.0)
+        self.ui_window.sp98E5LCD.display(0.0)
+        self.ui_window.sp95E10LCD.display(0.0)
+        self.ui_window.sp95E5PremiumLCD.display(0.0)
+        self.ui_window.sp98E10LCD.display(0.0)
+
+        self.ui_window.gasoleoALCD.display(0.0)
+        self.ui_window.gasoleoBLCD.display(0.0)
+        self.ui_window.gasoleoPremiumLCD.display(0.0)
+
+        self.ui_window.biodieselLCD.display(0.0)
+        self.ui_window.bioetanolLCD.display(0.0)
+
+        self.ui_window.gncLCD.display(0.0)
+        self.ui_window.gnlLCD.display(0.0)
+        self.ui_window.glpLCD.display(0.0)
+        self.ui_window.hidrogenoLCD.display(0.0)
 
 app = QtWidgets.QApplication([])
 application = MainWindow()
